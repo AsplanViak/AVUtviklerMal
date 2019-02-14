3d37c5267e86d41f3698ece76672b32e22d57c96
Andreas Glærum <aglaerum@gmail.com>
2019-02-14 21:19:40 +0100

    __beta__ satt til false offkurse

--- MalUtvidelse.extension/Mal.tab/TestSone.panel/Malknapp.pushbutton/malknapp_script.py
+++ MalUtvidelse.extension/Mal.tab/TestSone.panel/Malknapp.pushbutton/malknapp_script.py
@@ -18,17 +18,17 @@ Fungerende autocomplete i pycharm.
 # Start MÅ ha
 __title__ = 'Sjekk logging'  # Denne overstyrer navnet på scriptfilen
 __author__ = 'Asplan Viak'  # Dette kommer opp som navnet på utvikler av knappen
-__doc__ = "Sjekker om loggingsystemet fungerer"  # Dette blir hjelp teksten som kommer opp når man holder musepekeren over knappen.
+__doc__ = "Alt-klikk denne knappen og utforsk kildekoden for å lage egne knapper."  # Dette blir hjelp teksten som kommer opp når man holder musepekeren over knappen.
 # End MÅ ha
 
 # Kan sløyfes
 __cleanengine__ = False  # Dette forteller tolkeren at den skal sette opp en ny ironpython motor for denne knappen, slik at den ikke kommer i konflikt med andre funksjoner, settes nesten alltid til FALSE, TRUE når du jobber med knappen.
 __fullframeengine__ = False  # Denne er nødvendig for å få tilgang til noen moduler, denne gjør knappen vesentrlig tregere i oppstart hvis den står som TRUE
 __context__ = "zerodoc"  # Denne forteller tolkeren at knappen skal kunne brukes selv om et Revit dokument ikke er åpent.
-__helpurl__ = "whatever.com"  # Hjelp URL når bruker trykker F1 over knapp.
-__min_revit_ver__ = 2017  # knapp deaktivert hvis klient bruker lavere versjon
-__max_revit_ver__ = 2018  # Skjønner?
-__beta__ = True  # Knapp deaktivert hos brukere som ikke har spesifikt aktivert betaknapper
+__helpurl__ = "google.no"  # Hjelp URL når bruker trykker F1 over knapp.
+__min_revit_ver__ = 2010  # knapp deaktivert hvis klient bruker lavere versjon
+__max_revit_ver__ = 2032  # Skjønner?
+__beta__ = False  # Knapp deaktivert hos brukere som ikke har spesifikt aktivert betaknapper
 
 
 # Finn flere variabler her:
